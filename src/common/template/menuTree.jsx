da8410f3ab1b948058657cda1a61891bde5d13ff
Fernando Da Silva <dasilvaf@amdocs.com>
2020-05-17 22:03:40 -0300

    My Money app frontend implementation

---
+++ src/common/template/menuTree.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default props => (
+    <li className='treeview'>
+        <Link to={props.path}>
+            <i className={`fa fa-${props.icon}`}></i> <span>{props.label}</span>
+            <i className='fa fa-angle-left pull-right'></i>
+        </Link>
+        <ul className='treeview-menu'>
+            {props.children}
+        </ul>
+    </li>
+)
